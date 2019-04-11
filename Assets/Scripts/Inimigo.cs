722828a442f3a5f80dfd4b531d468219a11e07a9
Guilherme Galarça Dutra <udutra@gmail.com>
2019-04-11 18:18:01 -0300

    Aula 70 - CAW - cartoon air warfare - Parte 5
    Alteração do SortingLayer (Default na posição 0, Primeiro Plano na posição 1).
    Alteração no script Inimigo: Criação do delay entre os tiros do inimigo, criação do método Atirar e  criação do método Shot.

--- Assets/Scripts/Inimigo.cs
+++ Assets/Scripts/Inimigo.cs
@@ -8,21 +8,18 @@ public class Inimigo : MonoBehaviour
     public Transform arma;
     public GameObject explosaoPrefab, tiroPrefab;
     public GameObject[] loot;
+    public float delayEntreTiros;
    
 
     private void Start()
     {
         _playerController = FindObjectOfType(typeof(PlayerController)) as PlayerController;
+        StartCoroutine("Atirar");
     }
 
     private void Update()
     {
-        arma.right = _playerController.transform.position - transform.position;
-        if (Input.GetButtonDown("Fire2"))
-        {
-            GameObject temp = Instantiate(tiroPrefab, arma.position, arma.localRotation);
-            temp.GetComponent<Rigidbody2D>().velocity = arma.right * 3;
-        }
+        
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -65,5 +62,19 @@ public class Inimigo : MonoBehaviour
             }
             Instantiate(loot[idItem], transform.position, transform.localRotation);
         }
+    }
+
+    private IEnumerator Atirar()
+    {
+        yield return new WaitForSeconds(delayEntreTiros);
+        Shot();
+        StartCoroutine("Atirar");
+    }
+
+    private void Shot()
+    {
+        arma.right = _playerController.transform.position - transform.position;
+        GameObject temp = Instantiate(tiroPrefab, arma.position, arma.localRotation);
+        temp.GetComponent<Rigidbody2D>().velocity = arma.right * 3;
     }
 }
