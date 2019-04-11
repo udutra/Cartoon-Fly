d3192e964f94b26d7b89c3bbaf9fade041e1c37f
Guilherme Galarça Dutra <udutra@gmail.com>
2019-04-11 15:13:34 -0300

    Aula 66 - CAW - cartoon air warfare - Parte 1
    Adição dos Assets.
    Criação do Player.
    Criação do Script do Player: PlayerController.
    Criação da do 01_Bullet.
    Criação do Prefaba do 01_Bullet.
    Criação do Script do Shot ShotController.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D playerRb;
+    public float velocidade;
+    public GameObject bulletPrefab;
+    public Transform armaPosition;
+    public float velocidadeTiro;
+
+    private void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        playerRb.velocity = new Vector2(horizontal * velocidade, vertical * velocidade);
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shot();
+        }
+    }
+
+    private void Shot()
+    {
+        GameObject temp = Instantiate(bulletPrefab);
+        temp.transform.position = armaPosition.position;
+        temp.GetComponent<Rigidbody2D>().velocity = new Vector2(0,velocidadeTiro);
+    }
+}
