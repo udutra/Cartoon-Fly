ff9a722bde9c9cf032b76141fc6329c87f8dab29
Guilherme Galarça Dutra <udutra@gmail.com>
2019-04-18 18:16:50 -0300

    Aula 72 - CAW - cartoon air warfare - Parte 7.
    Criação dos Limites da Camera.
    Criação do Script Game Controller.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public PlayerController _PlayerController;
+
+    [Header("Limite de Movimento")]
+    public Transform limiteSuperior;
+    public Transform limiteInferior, limiteEsquerdo, limiteDireito;
+
+    [Header("Limite Lateral Camera")]
+    public Camera mainCamera;
+    public Transform limiteCamEsquerdo, limiteCamDireito;
+    public float velocidadeLateralCamera;
+
+    private void Start()
+    {
+        _PlayerController = FindObjectOfType(typeof(PlayerController)) as PlayerController;
+    }
+
+    private void Update()
+    {
+        LimitarMovimentoPlayer();
+    }
+
+    private void LateUpdate()
+    {
+        ControlePosicaoCamera();
+    }
+
+    private void LimitarMovimentoPlayer()
+    {
+        float posX = _PlayerController.transform.position.x;
+        float posY = _PlayerController.transform.position.y;
+
+
+        if(posY > limiteSuperior.position.y)
+        {
+            _PlayerController.transform.position = new Vector3(posX, limiteSuperior.position.y, 0);
+        }
+        else if(posY < limiteInferior.position.y)
+        {
+            _PlayerController.transform.position = new Vector3(posX, limiteInferior.position.y, 0);
+        }
+
+        if(posX > limiteDireito.position.x)
+        {
+            _PlayerController.transform.position = new Vector3(limiteDireito.position.x, posY, 0);
+        }
+        else if (posX < limiteEsquerdo.position.x)
+        {
+            _PlayerController.transform.position = new Vector3(limiteEsquerdo.position.x, posY, 0);
+        }
+    }
+
+    private void ControlePosicaoCamera()
+    {
+        if (mainCamera.transform.position.x > limiteCamEsquerdo.position.x && mainCamera.transform.position.x < limiteCamDireito.position.x)
+        {
+            MoverCamera();
+        }
+
+        else if(mainCamera.transform.position.x <= limiteCamEsquerdo.position.x && _PlayerController.transform.position.x > limiteCamEsquerdo.position.x)
+        {
+            MoverCamera();
+        }
+
+        else if (mainCamera.transform.position.x >= limiteCamDireito.position.x && _PlayerController.transform.position.x < limiteCamDireito.position.x)
+        {
+            MoverCamera();
+        }
+    }
+
+    private void MoverCamera()
+    {
+        Vector3 posicaoDestinoCamera = new Vector3(_PlayerController.transform.position.x, mainCamera.transform.position.y, mainCamera.transform.position.z);
+        mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, posicaoDestinoCamera, velocidadeLateralCamera * Time.deltaTime);
+    }
+}
