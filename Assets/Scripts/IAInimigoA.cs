6bd4f8ea1355b31f6199e1cc6e97a7160ecc71b0
Guilherme Galarça Dutra <udutra@gmail.com>
2019-04-23 15:24:58 -0300

    Aula 75 - CAW - cartoon air warfare - Parte 10
    Criação do Inimigo 2.
    Criação do IAInimigo.

---
+++ Assets/Scripts/IAInimigoA.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IAInimigoA : MonoBehaviour
+{
+    public float velocidadeMovimento, pontoInicialCurva, grausCurva, incrementar;
+    private float incrementado, rotacaoZ;
+    public bool isCurva;
+
+    private void Start()
+    {
+        rotacaoZ = transform.eulerAngles.z;
+    }
+
+    private void Update()
+    {
+        if (transform.position.y <= pontoInicialCurva && isCurva == false)
+        {
+            isCurva = true;
+        }
+        if(isCurva && incrementado < grausCurva)
+        {
+            rotacaoZ += incrementar;
+            transform.rotation = Quaternion.Euler(0, 0, rotacaoZ);
+            if(incrementar < 0)
+            {
+                incrementado += (incrementar * -1);
+            }
+            else
+            {
+                incrementado += incrementar;
+            }
+            
+        }
+        transform.Translate(Vector3.down * velocidadeMovimento * Time.deltaTime);
+
+    }
+}
