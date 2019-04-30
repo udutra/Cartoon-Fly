c655050e5d7a308d7510cfa5c7c38ebc17f93e70
Guilherme Galarça Dutra <udutra@gmail.com>
2019-04-30 18:05:54 -0300

    Aula 78 - CAW - cartoon air warfare - Parte 13.
    Alteração no Script do inimigo 02 para atirar.

--- Assets/Scripts/IAInimigo.cs
+++ Assets/Scripts/IAInimigo.cs
@@ -1,12 +1,22 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class IAInimigo : MonoBehaviour
 {
-    public float velocidadeMovimento, pontoInicialCurva, grausCurva, incrementar;
     private float incrementado, rotacaoZ;
+    public Direcao direcaoMovimento;
+
+    public float velocidadeMovimento, pontoInicialCurva;
     public bool isCurva;
+    public float grausCurva, incrementar;
+
+    public GameObject prefabTiro;
+    public Transform arma;
+    public float velocidadeTiro;
+
+    public float delayTiro;
 
     private void Start()
     {
@@ -15,15 +25,27 @@ public class IAInimigo : MonoBehaviour
 
     private void Update()
     {
+        ControleCurva();
+
+
+    }
+
+    private void OnBecameVisible()
+    {
+        StartCoroutine("ControleTiro");
+    }
+
+    private void ControleCurva()
+    {
         if (transform.position.y <= pontoInicialCurva && isCurva == false)
         {
             isCurva = true;
         }
-        if(isCurva && incrementado < grausCurva)
+        if (isCurva && incrementado < grausCurva)
         {
             rotacaoZ += incrementar;
             transform.rotation = Quaternion.Euler(0, 0, rotacaoZ);
-            if(incrementar < 0)
+            if (incrementar < 0)
             {
                 incrementado += (incrementar * -1);
             }
@@ -31,9 +53,21 @@ public class IAInimigo : MonoBehaviour
             {
                 incrementado += incrementar;
             }
-            
+
         }
         transform.Translate(Vector3.down * velocidadeMovimento * Time.deltaTime);
+    }
 
+    private void Atirar()
+    {
+        GameObject temp = Instantiate(prefabTiro, arma.position, transform.localRotation);
+        temp.GetComponent<Rigidbody2D>().velocity = transform.up * -1 * velocidadeTiro;
+    }
+
+    private IEnumerator ControleTiro()
+    {
+        yield return new WaitForSeconds(delayTiro);
+        Atirar();
+        StartCoroutine("ControleTiro");
     }
 }
